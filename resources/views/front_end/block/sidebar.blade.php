6808c5e972cae2808e7ebacb00365c8612907907
@hiepcm <hiepcm@imap.edu.vn>
2023-12-29 09:25:38 +0700

    up[date

--- resources/views/front_end/block/sidebar.blade.php
+++ resources/views/front_end/block/sidebar.blade.php
@@ -1,7 +1,7 @@
 <section id="calendar-3" class="widget widget_calendar">
     <div id="calendar_wrap" class="calendar_wrap">
-        <table id="wp-calendar" class="wp-calendar-table">
-            <caption>December 2023</caption>
+        <table id="wp-calendar" class="wp-calendar-table calendar">
+            <caption id="title_calendar">December 2023</caption>
             <thead>
                 <tr>
                     <th scope="col" title="Monday">M</th>
@@ -13,49 +13,7 @@
                     <th scope="col" title="Sunday">S</th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
-                    <td colspan="4" class="pad">&nbsp;</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>5</td>
-                    <td>6</td>
-                    <td>7</td>
-                    <td>8</td>
-                    <td>9</td>
-                    <td>10</td>
-                </tr>
-                <tr>
-                    <td>11</td>
-                    <td>12</td>
-                    <td>13</td>
-                    <td>14</td>
-                    <td>15</td>
-                    <td>16</td>
-                    <td>17</td>
-                </tr>
-                <tr>
-                    <td>18</td>
-                    <td>19</td>
-                    <td>20</td>
-                    <td>21</td>
-                    <td>22</td>
-                    <td>23</td>
-                    <td id="today">24</td>
-                </tr>
-                <tr>
-                    <td>25</td>
-                    <td>26</td>
-                    <td>27</td>
-                    <td>28</td>
-                    <td>29</td>
-                    <td>30</td>
-                    <td>31</td>
-                </tr>
+            <tbody id="calendar-body"> 
             </tbody>
         </table>  
     </div>
