29837779abb901531f3916e646f75bb8d30c4908
minhhiep02112001 <chuminhhiep0211@gmail.com>
2023-12-29 23:34:17 +0700

    u

--- resources/views/front_end/layout/config_seo_header.blade.php
+++ resources/views/front_end/layout/config_seo_header.blade.php
@@ -48,3 +48,12 @@
 <meta property="fb:admins" content="100036897402369" />
 
 <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3JDMVLT18T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3JDMVLT18T');
+</script>
+<!-- Google tag (gtag.js) -->
