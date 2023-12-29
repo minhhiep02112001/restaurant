41325d45d3df4d170beb63e0659ce6549e80076e
@hiepcm <hiepcm@imap.edu.vn>
2023-12-29 11:15:22 +0700

    update

--- resources/views/front_end/about.blade.php
+++ resources/views/front_end/about.blade.php
@@ -864,13 +864,13 @@
                         #PAGES_CONTAINER {
                             left:0; margin-left:0; width:100%; min-width:0;
                         }
-
+                        #comp-k3c14z5g {
+                            padding-top: 40px;
+                        }
                         #SITE_PAGES {
                             left:0; margin-left:0; width:100%; min-width:0;
                         }
-                        #SITE_PAGES {
-                            padding-top: 30px;
-                        }
+                        
                         #comp-kp09f54m {
                             width:5px; height:5px;
                         }
