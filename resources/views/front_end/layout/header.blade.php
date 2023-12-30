c0cb94189650c3cfa8b453a160dc0d224ac2d312
minhhiep02112001 <chuminhhiep0211@gmail.com>
2023-12-30 10:52:57 +0700

    update code

--- resources/views/front_end/layout/header.blade.php
+++ resources/views/front_end/layout/header.blade.php
@@ -1,28 +1,24 @@
 <header class="Header Header--bottom Header--overlay">
     <div class="Header-inner Header-inner--bottom" data-nc-group="bottom">
         <div data-nc-container="bottom-left">
-            <nav class="Header-nav Header-nav--primary" data-nc-element="primary-nav"
-                data-content-field="navigation">
+            <nav class="Header-nav Header-nav--primary" data-nc-element="primary-nav" data-content-field="navigation">
 
                 <div class="Header-nav-inner">
-                    <a href="index.html" class="Header-nav-item Header-nav-item--active"
-                        data-test="template-nav">Home</a><a href="kookbbqmenu.html"
-                        class="Header-nav-item" data-test="template-nav">Menu</a><a href="about.html"
-                        class="Header-nav-item" data-test="template-nav">About</a><a
-                        href="reservations.html" class="Header-nav-item"
-                        data-test="template-nav">Reservations</a><a href="contact.html"
-                        class="Header-nav-item" data-test="template-nav">Contact</a>
+                    <a href="/" class="Header-nav-item Header-nav-item--active" data-test="template-nav">Home</a>
+                    <a href="/menu" class="Header-nav-item" data-test="template-nav">Menu</a>
+                    <a href="/about" class="Header-nav-item" data-test="template-nav">About</a>
+                    <a href="/reservations" class="Header-nav-item" data-test="template-nav">Reservations</a>
+                    <a href="/contact" class="Header-nav-item" data-test="template-nav">Contact</a>
                 </div>
 
             </nav>
         </div>
         <div data-nc-container="bottom-center">
 
-            <a href="index.html" class="Header-branding" data-nc-element="branding"
-                data-content-field="site-title">
+            <a href="index.html" class="Header-branding" data-nc-element="branding" data-content-field="site-title">
                 <img src="../images.squarespace-cdn.com/content/v1/5c4766f7ee175955203ef26a/1588627323969-8A99TT8UPKP5I543JTY3/kook-logo-gold-0126f7.png?format=1500w"
-                    alt="Kook Korean BBQ" class="Header-branding-logo" /> 
-            </a>  
+                    alt="Kook Korean BBQ" class="Header-branding-logo" />
+            </a>
         </div>
         <div data-nc-container="bottom-right">
             <nav class="Header-nav Header-nav--secondary" data-nc-element="secondary-nav"
